2f9d5b3146b21ea8553c5a96fecec2910667015f
aysekarapinar <ayseozlemkarapinar28@gmail.com>
2024-05-10 00:37:43 +0300

    cookiedicker action

--- cookiedicker.py
+++ cookiedicker.py
@@ -1,3 +1,8 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+browser.maximize_window()
+browser.get("https://orteil.dashnet.org/cookieclicker/")
+sleep(5)
