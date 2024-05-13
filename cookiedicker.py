71f991541482b97c8448e627b60385440548f358
Ayşe Karapinar <164161988+aysekarapinar@users.noreply.github.com>
2024-05-14 02:07:30 +0300

    Delete cookiedicker.py

--- cookiedicker.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from time import sleep
-from selenium.webdriver.common.by import By
-
-browser = webdriver.Chrome()
-browser.maximize_window()
-browser.get("https://orteil.dashnet.org/cookieclicker/")
-sleep(5)
